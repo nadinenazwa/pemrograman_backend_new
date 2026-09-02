5871b532037258a1b500c6414da5a3649d22c8a2
nadinenazwa <nadinenazwaandina@gmail.com>
2026-09-02 19:54:12 +0700

    feat: implementasi variabel, slice, dan map

--- 1-latihan-fiber/main.go
+++ 1-latihan-fiber/main.go
@@ -1,16 +1,22 @@
 package main
 
-import (
-	"log"
-	"github.com/gofiber/fiber/v2"
-)
+import "fmt"
 
 func main() {
-	app := fiber.New()
+	fmt.Println("=== SOAL 2: VARIABEL & MAP ===")
+	var nama string = "Nadine Nazwa Andina"
+	var umur int = 20
+	var ipk float64 = 3.85
+	var isAktif bool = true
+	var matkul []string = []string{"Pemrograman Backend", "Basis Data"}
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+	fmt.Printf("Nama: %s\nUmur: %d\nIPK: %.2f\nAktif: %v\nMatkul: %v\n\n", nama, umur, ipk, isAktif, matkul)
 
-	log.Fatal(app.Listen(":3000"))
+	nilaiMhs := make(map[string]float64)
+	nilaiMhs["Nadine Nazwa Andina"] = 88.5
+	nilaiMhs["Adam Ahmad Bimantoro"] = 92.0
+
+	for nama, nilai := range nilaiMhs {
+		fmt.Printf("- %s : %.2f\n", nama, nilai)
+	}
 }
